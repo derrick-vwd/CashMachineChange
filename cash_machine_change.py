359938205f69014f5f03d6daf492d611b1f4c7ae
eugene <derrick.thornton.dev@gmail.com>
2026-08-10 10:11:12 -0500

    Keeps asking for payment if the payment is less than cost.

--- cash_machine_change.py
+++ cash_machine_change.py
@@ -2,31 +2,33 @@
 to give back in coins and notes, using only arithmetic ops."""
 
 total_cost = float(input('Total cost: '))
-customer_payment = float(input('Payment: '))
 
-change = customer_payment - total_cost
-# Assumes that the customer will pay the entire sum.
+while True:
+	customer_payment = float(input('Payment: '))
+	change = customer_payment - total_cost	
 
-if change > 0:
+	if change > 0:
 
-	cents = change % 1
-	dollars = change - cents
+		cents = change % 1
+		dollars = change - cents
 
-	quarters = cents // .25
-	cents = round(cents % .25, 2)
+		quarters = cents // .25
+		cents = round(cents % .25, 2)
 
-	dimes = cents // 0.10
-	cents = round(cents % .10, 2)
+		dimes = cents // 0.10
+		cents = round(cents % .10, 2)
 
-	nickels = cents // .05
-	cents = round(cents % .05, 2)
+		nickels = cents // .05
+		cents = round(cents % .05, 2)
 
-	pennies = cents // 0.01
+		pennies = cents // 0.01
 
-	print('Dollars: ' + str(int(dollars)))
-	print('Quarters: ' + str(int(quarters)))
-	print('Dimes: ' + str(int(dimes)))
-	print('Nickels: ' + str(int(nickels)))
-	print('Pennies: ' + str(int(pennies)))
-else:
-	print('You didn\'t provide enough money.')
+		print('Dollars: ' + str(int(dollars)))
+		print('Quarters: ' + str(int(quarters)))
+		print('Dimes: ' + str(int(dimes)))
+		print('Nickels: ' + str(int(nickels)))
+		print('Pennies: ' + str(int(pennies)))
+
+		break
+	else:
+		print('You didn\'t provide enough money. Reassess and pay.')
